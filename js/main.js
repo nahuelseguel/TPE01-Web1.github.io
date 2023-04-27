66fcf1336c17c6c85ccbdc15a5b23d4ca6098612
Pablo Demateo <63564990+demateopablo@users.noreply.github.com>
2023-04-26 21:38:25 -0300

    Updated submitButton click event
    Ahora, si el captcha es correcto, evita la ejecucion del submit para poder mostrar un label bajo el form solo a modo de ejemplo para la actividad.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 "use strict";
+
 let captcha = document.getElementById("captcha");  //Span donde se genera el captcha
 let randomCaptcha;
 let submitButton = document.getElementById("submitButton"); //Boton que hace la validacion
@@ -14,16 +15,16 @@ function generateCaptcha() {
 
 function validateCaptcha(event) {
     let userInput = document.getElementById("captchaInput");   //Input text donde el usuario ingresa la respuesta
-    let userInputValue = userInput.value;
+    let userInputValue = userInput.value;  //Lo que ingresó el user
     let isValid = document.getElementById("label-IsValidForm");  //Label donde se muestra el resultado
     if (captcha.innerHTML != userInputValue) {
         event.preventDefault();
         isValid.innerHTML = "🤖Captcha incorrecto🤖. Intentá nuevamente.";
-        generateCaptcha();
         userInput.value = "";
     }
     else {
         isValid.innerHTML = "Captcha correcto! No sos un robot.";
+        event.preventDefault();
     }
     //lo muestra en consola (opcional, para debug)
     console.log(isValid.innerHTML);
