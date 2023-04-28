491b543e5cb859cb3321efa6c4df85121558c40e
Pablo Demateo <63564990+demateopablo@users.noreply.github.com>
2023-04-27 23:13:23 -0300

    Overlay click event listening added

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@ const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
 const overlay = document.querySelector('.overlay');
 
-
 menuIcon.addEventListener('click', function () {
     menu.classList.toggle('show');
     overlay.classList.toggle('show');
     document.body.classList.toggle('no-scroll');
 });
+
+overlay.addEventListener('click', function () {
+    menu.classList.toggle('show');
+    overlay.classList.toggle('show');
+    document.body.classList.toggle('no-scroll');
+});
+
