f748c849c127e7963e2f09a437f5fd7ec58625e6
Pablo Demateo <63564990+demateopablo@users.noreply.github.com>
2023-05-15 20:04:59 -0300

    Add files via upload

--- js/board.js
+++ js/board.js
@@ -1,28 +1,45 @@
+
+let botonMes = document.querySelector("#boton-mes");
+botonMes.addEventListener("click", colocarMes);
+let monthSelector = document.querySelector("#mes");
+let tabla = document.querySelector("#tabla1");
+
+function colocarMes(e) {
+    e.preventDefault();
+    colocarMesIngresado();
+    alternarAVisible();
+  }
+
 // guarda el valor de la fecha y lo muestra en la tabla
 function colocarMesIngresado () {
-    
-    let cuerpoTabla = document.getElementById("tabla1").getElementsByTagName('tbody')[0];
-    
-    let filaMes = cuerpoTabla.insertRow();
-    cuerpoTabla.deleteRow(0);
-    let celda = filaMes.insertCell(0);
-    let travelDateFrom = document.getElementById("mes").value;
-    celda.innerHTML = travelDateFrom;
-    
+  let cuerpoTabla = document.getElementById("tabla1").getElementsByTagName('tbody')[0];
+  
+  let filaMes = cuerpoTabla.insertRow();
+  cuerpoTabla.deleteRow(0);
+  let celda = filaMes.insertCell(0);
+  let travelDateFrom = document.getElementById("mes").value;
+  celda.innerHTML = travelDateFrom;
 }
 
-let botonMes = document.getElementById("boton-mes");
-botonMes.addEventListener("click", function(event) {
-    event.preventDefault();
-    colocarMesIngresado();
-  });
+function alternarAVisible() {
+  if(mes.value==""){
+  tabla.classList.add("oculto");
+}
+else {
+  tabla.classList.toggle("oculto");
 
-  // alternar a visible los elementos de la tabla
+}
 
-  document.querySelector("#boton-mes").addEventListener("click", function alternarAVisible() {
+
+}
+
+
+/*   // alternar a visible los elementos de la tabla
+
+  document.querySelector("#boton-mes").addEventListener("click", 
     
-    document.querySelector("tr").classList.toggle("oculto");
+
 });
     
   
-  
+   */
